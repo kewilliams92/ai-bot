604afd5ae6148e9fb640b9663b11c029f5b65349
Kevin Williams <kewilliamsdev@gmail.com>
2025-12-01 15:44:49 +0300

    AI bot beginning

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No input was provided")
+        return 1
+
+    user_input = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_input)])
+    ]
+    response =client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_input}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
